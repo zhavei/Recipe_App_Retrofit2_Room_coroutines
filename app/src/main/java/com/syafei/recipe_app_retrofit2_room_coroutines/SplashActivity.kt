49f527501657bc16555886c08e2bd801cf4fde8c
zhavei <zhavei.vei@gmail.com>
2022-10-12 16:16:09 +0700

    Initial commit

---
+++ app/src/main/java/com/syafei/recipe_app_retrofit2_room_coroutines/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.syafei.recipe_app_retrofit2_room_coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
