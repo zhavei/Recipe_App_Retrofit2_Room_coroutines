366002ef2b95547c054793de2d718c9330d76115
zhavei <zhavei.vei@gmail.com>
2022-10-26 15:59:16 +0700

    create home xml

---
+++ app/src/main/java/com/syafei/recipe_app_retrofit2_room_coroutines/ui/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.syafei.recipe_app_retrofit2_room_coroutines.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
