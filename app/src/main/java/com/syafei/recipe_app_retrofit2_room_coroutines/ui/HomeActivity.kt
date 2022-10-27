80e9467779c7ab17df66702e41a77e0fa2eb3c3c
zhavei <zhavei.vei@gmail.com>
2022-10-27 14:27:49 +0700

    oke

--- app/src/main/java/com/syafei/recipe_app_retrofit2_room_coroutines/ui/HomeActivity.kt
+++ app/src/main/java/com/syafei/recipe_app_retrofit2_room_coroutines/ui/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.syafei.recipe_app_retrofit2_room_coroutines.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.syafei.recipe_app_retrofit2_room_coroutines.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/syafei/recipe_app_retrofit2_room_coroutines/ui/SplashActivity.kt
+++ app/src/main/java/com/syafei/recipe_app_retrofit2_room_coroutines/ui/SplashActivity.kt
@@ -1,12 +1,34 @@
 package com.syafei.recipe_app_retrofit2_room_coroutines.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.syafei.recipe_app_retrofit2_room_coroutines.R
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.syafei.recipe_app_retrofit2_room_coroutines.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        binding.btnGetStarted.setOnClickListener {
+
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+        }
+
     }
 }
